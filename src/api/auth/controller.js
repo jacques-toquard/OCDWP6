cdfb8af81fe59bbca6e65288d16ce1239f08bedb
Jacques Toquard <jacques.toquard@proton.me>
2025-01-09 03:57:21 +0100

    style: use modern es syntax

--- src/api/auth/controller.js
+++ src/api/auth/controller.js
@@ -3,45 +3,37 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 const saltRounds = 10;
-
 const OCDWP6_SECRET_KEY = process.env.OCDWP6_SECRET_KEY;
 
-export const signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, saltRounds)
-    .then(hash => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      user
-        .save()
-        .then(() => res.status(201).json({ message: 'Utilisateur crée' }))
-        .catch(error => res.status(500).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
+export const signup = async (req, res, next) => {
+  try {
+    const user = new User({
+      email: req.body.email,
+      password: await bcrypt.hash(req.body.password, saltRounds),
+    });
+    await user.save();
+    res.status(201).json({ message: 'Utilisateur crée' });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
-export const login = (req, res, next) => {
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé' });
-      }
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then(valid => {
-          if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect' });
-          }
-          res.status(200).json({
-            userId: user._id,
-            token: `Bearer ${jwt.sign({ userId: user._id }, OCDWP6_SECRET_KEY, {
-              expiresIn: '7d',
-            })}`,
-          });
-        })
-        .catch(error => res.status(500).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(401).json({ error: 'Utilisateur non trouvé' });
+    }
+    if (!(await bcrypt.compare(req.body.password, user.password))) {
+      return res.status(401).json({ error: 'Mot de passe incorrect' });
+    }
+    res.status(200).json({
+      userId: user._id,
+      token: `Bearer ${jwt.sign({ userId: user._id }, OCDWP6_SECRET_KEY, {
+        expiresIn: '7d',
+      })}`,
+    });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
