5d4b8ac9d4e43850be6a428bc59161486b92672f
Jacques Toquard <jacques.toquard@proton.me>
2025-01-09 02:35:33 +0100

    feat(api): add templates for all /books endpoints

--- src/api/books/controller.js
+++ src/api/books/controller.js
@@ -2,10 +2,10 @@ import Book from '../../models/Book.js';
 
 export const getAllBooks = (req, res, next) => {
   Book.find()
-    .then((books) => {
+    .then(books => {
       res.status(200).json(books);
     })
-    .catch((error) => {
+    .catch(error => {
       res.status(500).json({ error });
     });
 };
@@ -13,13 +13,13 @@ export const getAllBooks = (req, res, next) => {
 export const getBookById = (req, res, next) => {
   const bookId = req.params.id;
   Book.findById(bookId)
-    .then((book) => {
+    .then(book => {
       if (!book) {
         return res.status(404).json({ message: 'Livre non trouvé' });
       }
       res.status(200).json(book);
     })
-    .catch((error) => {
+    .catch(error => {
       res.status(500).json({ error });
     });
 };
@@ -29,8 +29,32 @@ export const getBestRatedBooks = (req, res, next) => {
   // todo
 };
 
+export const createBook = (req, res, next) => {
+  res.status(202).json({ message: 'Not implemented' });
+  // todo
+};
+
+export const rateBook = (req, res, next) => {
+  res.status(202).json({ message: 'Not implemented' });
+  // todo
+};
+
+export const updateBook = (req, res, next) => {
+  res.status(202).json({ message: 'Not implemented' });
+  // todo
+};
+
+export const deleteBook = (req, res, next) => {
+  res.status(202).json({ message: 'Not implemented' });
+  // todo
+};
+
 export default {
   getAllBooks,
   getBookById,
   getBestRatedBooks,
+  createBook,
+  rateBook,
+  updateBook,
+  deleteBook,
 };
--- src/api/books/index.js
+++ src/api/books/index.js
@@ -1,10 +1,26 @@
 import express from 'express';
-import { getAllBooks, getBookById, getBestRatedBooks } from './controller.js';
+import auth from '../../middlewares/auth.js';
+import {
+  getAllBooks,
+  getBookById,
+  getBestRatedBooks,
+  createBook,
+  rateBook,
+  updateBook,
+  deleteBook,
+} from './controller.js';
 
 const booksRouter = express.Router();
 
 booksRouter.get('/', getAllBooks);
-booksRouter.get('/bestrating', getBestRatedBooks);
+booksRouter.get('/bestrating', getBestRatedBooks); // todo
 booksRouter.get('/:id', getBookById);
+
+booksRouter.post('/', auth, createBook); // todo
+booksRouter.post('/:id/rating', auth, rateBook); // todo
+
+booksRouter.put('/:id', auth, updateBook); // todo
+
+booksRouter.delete('/:id', auth, deleteBook); // todo
 
 export default booksRouter;
