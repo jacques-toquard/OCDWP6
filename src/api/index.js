d8e3ce8efbfdc026eb2e11b49b8c5e22b0eb2cc5
Jacques Toquard <jacques.toquard@proton.me>
2025-01-09 03:09:48 +0100

    refactor: remove test endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,10 @@
 import express from 'express';
-// todo: import authRoutes from './auth/index.js';
+import authRoutes from './auth/index.js';
 import booksRoutes from './books/index.js';
 
 const apiRouter = express.Router();
 
-apiRouter.get('/test', (req, res) => {
-  res.json({ message: 'Test' });
-});
-
-// todo: apiRouter.use('/auth', authRoutes);
+apiRouter.use('/auth', authRoutes);
 apiRouter.use('/books', booksRoutes);
 
 export default apiRouter;
