84039d7e837bb53462846a7aba947c1ca88b76ec
Jacques Toquard <jacques.toquard@proton.me>
2025-01-09 03:04:19 +0100

    feat(middlewares): add User validation middlewares

---
+++ src/middlewares/validateSignupUser.js
@@ -0,0 +1,16 @@
+import { userSchema } from '../schemas/User.js';
+
+export default (req, res, next) => {
+  const { email, password } = req.body;
+  const errors = [];
+  if (!email || !userSchema.email.pattern.test(email)) {
+    errors.push("Le format de l'email est invalide");
+  }
+  if (!password || password.length < userSchema.password.minLength) {
+    errors.push('Le format du mot de passe est invalide');
+  }
+  if (errors.length > 0) {
+    return res.status(400).json({ errors });
+  }
+  next();
+};
