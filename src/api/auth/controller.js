10673e160e28b3ef36f91be8c312316dfe478e1e
Jacques Toquard <jacques.toquard@proton.me>
2025-01-09 03:47:21 +0100

    feat(auth): implement login and token bearer

--- src/api/auth/controller.js
+++ src/api/auth/controller.js
@@ -1,4 +1,8 @@
 import User from '../../models/User.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const OCDWP6_SECRET_KEY = process.env.OCDWP6_SECRET_KEY;
 
 export const signup = (req, res, next) => {
   res.status(202).json({ message: 'Not implemented' });
@@ -6,6 +10,25 @@ export const signup = (req, res, next) => {
 };
 
 export const login = (req, res, next) => {
-  res.status(202).json({ message: 'Not implemented' });
-  // todo
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non trouvé' });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect' });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: `Bearer ${jwt.sign({ userId: user._id }, OCDWP6_SECRET_KEY, {
+              expiresIn: '7d',
+            })}`,
+          });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
